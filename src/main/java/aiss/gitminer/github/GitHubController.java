993bbcf21bc85734bb43d46e2e184167d5b9d296
mimbrero <mimbrero.alberto@gmail.com>
2023-05-08 17:56:59 +0200

    feat: create GitHubController

---
+++ src/main/java/aiss/gitminer/github/GitHubController.java
@@ -0,0 +1,45 @@
+package aiss.gitminer.github;
+
+import aiss.gitminer.github.service.ProjectService;
+import aiss.gitminer.model.Project;
+import aiss.gitminer.service.GitMinerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class GitHubController {
+
+    @Autowired private GitMinerService gitMinerService;
+    @Autowired private ProjectService projectService;
+
+    @GetMapping("/{owner}/{repo}")
+    @ResponseStatus(HttpStatus.OK)
+    public Project get(@RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorization,
+                       @PathVariable String owner,
+                       @PathVariable String repo,
+                       @RequestParam(defaultValue = "2") int sinceCommits,
+                       @RequestParam(defaultValue = "20") int sinceIssues,
+                       @RequestParam(defaultValue = "2") int maxPages) {
+        String token = Optional.ofNullable(authorization)
+                .map(s -> s.replace("Bearer ", ""))
+                .orElse(null);
+        return this.projectService.findById(owner, repo, sinceCommits, sinceIssues, maxPages, token);
+    }
+
+    @PostMapping("/{owner}/{repo}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project create(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization,
+                          @PathVariable String owner,
+                          @PathVariable String repo,
+                          @RequestParam(defaultValue = "2") int sinceCommits,
+                          @RequestParam(defaultValue = "20") int sinceIssues,
+                          @RequestParam(defaultValue = "2") int maxPages) {
+        Project project = this.get(authorization, owner, repo, sinceCommits, sinceIssues, maxPages);
+        this.gitMinerService.uploadProject(project);
+        return project;
+    }
+}
