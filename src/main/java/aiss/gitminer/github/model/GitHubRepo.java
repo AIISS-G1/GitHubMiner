fae29fcb9285899c0d6746a7341d8f923af35811
mimbrero <mimbrero.alberto@gmail.com>
2023-05-08 17:53:32 +0200

    fix: add commits and issues fields

--- src/main/java/aiss/gitminer/github/model/GitHubRepo.java
+++ src/main/java/aiss/gitminer/github/model/GitHubRepo.java
@@ -1,9 +1,14 @@
 package aiss.gitminer.github.model;
 
+import aiss.gitminer.model.Commit;
+import aiss.gitminer.model.Issue;
 import aiss.gitminer.model.Project;
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.annotation.Generated;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("jsonschema2pojo")
@@ -17,6 +22,12 @@ public class GitHubRepo {
 
     @JsonProperty("html_url")
     private String htmlUrl;
+
+    @JsonIgnore
+    private List<Commit> commits;
+
+    @JsonIgnore
+    private List<Issue> issues;
 
     @JsonProperty("id")
     public Integer getId() {
@@ -48,8 +59,24 @@ public class GitHubRepo {
         this.htmlUrl = htmlUrl;
     }
 
+    public List<Commit> getCommits() {
+        return commits;
+    }
+
+    public void setCommits(List<Commit> commits) {
+        this.commits = commits;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<Issue> issues) {
+        this.issues = issues;
+    }
+
     public Project toProject() {
-        return new Project(this.id.toString(), this.name, this.htmlUrl, null, null);
+        return new Project(this.id.toString(), this.name, this.htmlUrl, this.commits, this.issues);
     }
 
     @Override
