c42cb6c3c53ae3a7225b22455aea7d89c7cc96b2
mimbrero <mimbrero.alberto@gmail.com>
2023-05-10 13:59:52 +0200

    test: create GitHubMinerApplicationTest

---
+++ src/test/java/aiss/gitminer/github/GitHubMinerApplicationTest.java
@@ -0,0 +1,29 @@
+package aiss.gitminer.github;
+
+import aiss.gitminer.authentication.AuthenticationRestTemplate;
+import aiss.gitminer.service.GitMinerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class GitHubMinerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void hasRestTemplateConfigured(ApplicationContext context) {
+        assertThat(context.getBean(RestTemplate.class)).isNotNull();
+    }
+
+    @Test
+    void hasBaseServicesConfigured(ApplicationContext context) {
+        assertThat(context.getBean(AuthenticationRestTemplate.class)).isNotNull();
+        assertThat(context.getBean(GitMinerService.class)).isNotNull();
+    }
+}
